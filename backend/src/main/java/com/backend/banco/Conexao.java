8b0e1ae30c32458cbc445285d6dc7b7e6a8c96aa
gustavorizeriodev <gustavogrizeriog@gmail.com>
2024-11-20 17:28:33 -0300

    fix: Arrumando conexão do banco de dados

--- backend/src/main/java/com/backend/banco/Conexao.java
+++ backend/src/main/java/com/backend/banco/Conexao.java
@@ -7,22 +7,17 @@ import javax.sql.DataSource;
 
 public class Conexao {
 
-    // Ambiente em Produção
     private final DataSource dataSource;
+    public Conexao(){
 
-    public Conexao() {
         BasicDataSource basicDataSource = new BasicDataSource();
-
-        String dbUrl = System.getenv("DB_URL");
-        String dbUser = System.getenv("DB_USER");
-        String dbPassword = System.getenv("DB_PASSWORD");
-
         basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        basicDataSource.setUrl(dbUrl != null ? dbUrl : "jdbc:mysql://172.18.0.1:3306/Vertex");
-        basicDataSource.setUsername(dbUser != null ? dbUser : "root");
-        basicDataSource.setPassword(dbPassword != null ? dbPassword : "urubu100");
+        basicDataSource.setUrl("jdbc:mysql://container-bd-vertex:3306/Vertex");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("urubu100");
 
         this.dataSource = basicDataSource;
+
     }
 
     public JdbcTemplate getConnection() {
